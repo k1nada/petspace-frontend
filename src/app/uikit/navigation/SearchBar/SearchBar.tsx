1e1ce6782e992948695c89179db7dc746c058ee8
szczurdada <natalyabel99@gmail.com>
2026-05-18 19:47:07 +0200

    feature/86c9vhyat-FE-tip-component

--- src/app/uikit/navigation/SearchBar/SearchBar.tsx
+++ src/app/uikit/navigation/SearchBar/SearchBar.tsx
@@ -1,18 +1,26 @@
+"use client";
+
 import { FaSearch } from "react-icons/fa";
 import styles from "./SearchBar.module.scss";
 import { useTranslations } from "next-intl";
 import { Input } from "../../form/Input/Input";
 
-export const SearchBar = () => {
+interface SearchBarProps {
+  placeholder?: string;
+  onChange?: (value: string) => void;
+}
+
+export const SearchBar = ({ placeholder, onChange }: SearchBarProps) => {
   const t = useTranslations();
 
   return (
-    <search className={styles.searchWrapper} role="search">
-      <FaSearch className={styles.searchIcon} />
+    <search className={styles.wrapper} role="search">
+      <FaSearch className={styles.icon} />
       <Input
         type="search"
-        placeholder={t("searchBar.search")}
+        placeholder={placeholder ?? t("searchBar.search")}
         appearance="search"
+        onChange={(e) => onChange?.(e.target.value)}
       />
     </search>
   );
